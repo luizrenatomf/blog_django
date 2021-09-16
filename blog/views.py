74951520ce9d7015b975c77e5468dc609941403b
luizrenatomf <luizrenatomf@gmail.com>
2021-09-16 20:47:30 -0300

    Commit inicial

---
+++ blog/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .models import Post
+
+def post_list(request):
+	object_list = Post.published.all()
+	paginator = Paginator(object_list, 3) # três postagens em cada página
+	page = request.GET.get('page')
+	try:
+		posts = paginator.page(page)
+	except PageNotAnInteger:
+		# Se a página não for um inteiro, exibe a primeira página
+		posts = paginator.page(1)
+	except EmptyPage:
+		# Se a página estiver fora do intervalo,
+		# exibe a última página de resultados
+		posts = paginator.page(paginator.num_pages)
+	return render(request,
+		          'blog/post/list.html',
+		          {'posts':posts})
+
+
+def post_detail(request,year,month,day,post):
+	post = get_object_or_404(Post,slug=post,
+		                     status='published',
+		                     publish__year=year,
+		                     publish__month=month,
+		                     publish__day=day)
+	return render(request,'blog/post/detail.html',{'post':post})
