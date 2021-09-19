014212abfd141b33dfb7e3a234edc65bbbb5a687
luizrenatomf <luizrenatomf@gmail.com>
2021-09-19 10:32:55 -0300

    Adiciona sistema de marcação por tags

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,16 @@ from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger 
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from taggit.models import Tag
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 
-def post_list(request):
+def post_list(request, tag_slug=None):
 	object_list = Post.published.all()
+	tag = None
+	if tag_slug:
+		tag = get_object_or_404(Tag,slug=tag_slug)
+		object_list = object_list.filter(tags__in=[tag])
 	paginator = Paginator(object_list, 3) # três postagens em cada página
 	page = request.GET.get('page')
 	try:
@@ -18,7 +23,7 @@ def post_list(request):
 		# Se a página estiver fora do intervalo,
 		# exibe a última página de resultados
 		posts = paginator.page(paginator.num_pages)
-	return render(request,'blog/post/list.html',{'page':page,'posts':posts})
+	return render(request,'blog/post/list.html',{'page':page,'posts':posts,'tag':tag})
 
 
 def post_detail(request,year,month,day,post):
